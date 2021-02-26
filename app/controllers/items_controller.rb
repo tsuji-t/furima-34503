52bea9d00996d32c0d0c737d7a638400acc33d99
tsuji-t <tsuji6911tomoki@gmail.com>
2021-02-26 17:59:19 +0900

    Revert "商品詳細機能"
    This reverts commit ea92c8a21c5c7fbf48c47a5f15b46c80821a11d8.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @items = Item.order("created_at DESC")
@@ -17,11 +17,6 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
-  def show
-    @item = Item.find(params[:id])
-  end
-
 
   private
   
